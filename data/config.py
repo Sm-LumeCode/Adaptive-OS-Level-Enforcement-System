2eea967991a034d25727bf7e923221062d525d01
Surabhi M <75206708+Sm-LumeCode@users.noreply.github.com>
2025-12-27 18:53:07 +0530

    Add configuration for mode and allowed/blocked apps

---
+++ data/config.py
@@ -0,0 +1,7 @@
+MODE = "normal"
+
+# Terminal ONLY allowed in exam
+WHITELIST = ["terminal","text editor"]
+
+# Block these in focus
+BLACKLIST = ["app center", "settings", "help", "files", "trash"]
